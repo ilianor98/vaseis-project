f3045cf8edd687beacf1a6f80ab0243c8a66c241
ilianor98 <112353715+ilianor98@users.noreply.github.com>
2023-05-15 16:14:58 +0300

    Update app.py

--- html/render/app.py
+++ html/render/app.py
@@ -1,56 +1,72 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 import pymysql
 
 app = Flask(__name__)
 
+# Set the secret key for the app
+app.secret_key = 'your_secret_key_here'
+
 @app.route('/')
 def index():
-    # Connect to the database
-    conn = pymysql.connect(
-        host='localhost',
-        user='root',
-        password='2403',
-        db='vaseistest1',
-        charset='utf8mb4',
-        cursorclass=pymysql.cursors.DictCursor
-    )
+    # Check if the user is logged in
+    if 'username' in session:
+        # Connect to the database
+        conn = pymysql.connect(
+            host='localhost',
+            user='root',
+            password='2403',
+            db='vaseistest1',
+            charset='utf8mb4',
+            cursorclass=pymysql.cursors.DictCursor
+        )
 
-    # Execute the query to retrieve data from the database
-    with conn.cursor() as cursor:
-        sql = 'SELECT * FROM user'
-        cursor.execute(sql)
-        data = cursor.fetchall()
+        # Execute the query to retrieve data from the database
+        with conn.cursor() as cursor:
+            sql = 'SELECT * FROM user'
+            cursor.execute(sql)
+            data = cursor.fetchall()
 
-    # Close the database connection
-    conn.close()
+        # Close the database connection
+        conn.close()
 
-    # Render the HTML template and pass the data to it
-    return render_template('index.html', data=data)
+        # Render the HTML template and pass the data to it
+        return render_template('index.html', data=data)
+    else:
+        return redirect(url_for('login'))
 
 @app.route('/student')
 def about():
-    # Connect to the database
-    conn = pymysql.connect(
-        host='localhost',
-        user='root',
-        password='2403',
-        db='vaseistest1',
-        charset='utf8mb4',
-        cursorclass=pymysql.cursors.DictCursor
-    )
+    # Check if the user is logged in
+    if 'username' in session:
+        # Connect to the database
+        conn = pymysql.connect(
+            host='localhost',
+            user='root',
+            password='2403',
+            db='vaseistest1',
+            charset='utf8mb4',
+            cursorclass=pymysql.cursors.DictCursor
+        )
 
-    # Execute the query to retrieve data from the database
-    with conn.cursor() as cursor:
-        sql = 'SELECT * FROM student'
-        cursor.execute(sql)
-        data = cursor.fetchall()
+        # Execute the query to retrieve data from the database
+        with conn.cursor() as cursor:
+            sql = 'SELECT * FROM student'
+            cursor.execute(sql)
+            data = cursor.fetchall()
 
-    # Close the database connection
-    conn.close()
-    return render_template('student.html', data=data)
+        # Close the database connection
+        conn.close()
+
+        return render_template('student.html', data=data)
+    else:
+        return redirect(url_for('login'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    # Check if the user is already logged in
+    if 'username' in session:
+        return redirect(url_for('index'))
+
     if request.method == 'POST':
         # Connect to the database
         conn = pymysql.connect(
@@ -76,11 +92,28 @@ def login():
         conn.close()
 
         if result:
-            # If user exists, redirect to home page
+            # If user exists, store username in session and redirect to home page
+            session['username'] = username
             return redirect(url_for('index'))
         else:
             # If user does not exist, redirect back to login page with error message
             return render_template('login.html', error='Invalid username or password')
     else:
-        # render login page
         return render_template('login.html')
+
+#@app.route('/logout', methods=['POST'])
+#def logout():
+ #   session.clear()
+  #  return redirect(url_for('logout'))
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    session.pop('username', None)
+    flash('You were logged out')
+    return redirect(url_for('login'))
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
