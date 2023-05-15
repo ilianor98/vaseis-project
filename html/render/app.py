eb9cf1a68eb8a09280e7bb2a201e27a0e33c019b
ilianor98 <112353715+ilianor98@users.noreply.github.com>
2023-05-15 13:53:35 +0300

    Update app.py

--- html/render/app.py
+++ html/render/app.py
@@ -3,7 +3,6 @@ import pymysql
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     # Connect to the database
@@ -11,7 +10,7 @@ def index():
         host='localhost',
         user='root',
         password='2403',
-        db='vaseistest',
+        db='vaseistest1',
         charset='utf8mb4',
         cursorclass=pymysql.cursors.DictCursor
     )
@@ -28,9 +27,6 @@ def index():
     # Render the HTML template and pass the data to it
     return render_template('index.html', data=data)
 
-# expretiment
-
-
 @app.route('/student')
 def about():
     # Connect to the database
@@ -38,7 +34,7 @@ def about():
         host='localhost',
         user='root',
         password='2403',
-        db='vaseistest',
+        db='vaseistest1',
         charset='utf8mb4',
         cursorclass=pymysql.cursors.DictCursor
     )
@@ -53,24 +49,38 @@ def about():
     conn.close()
     return render_template('student.html', data=data)
 
-
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    db = pymysql.connect(host='localhost', user='username',
-                         password='password', database='vaseistest')
-    # Get username and password from form submission
-    username = request.form['username']
-    password = request.form['password']
+    if request.method == 'POST':
+        # Connect to the database
+        conn = pymysql.connect(
+            host='localhost',
+            user='root',
+            password='2403',
+            db='vaseistest1',
+            charset='utf8mb4',
+            cursorclass=pymysql.cursors.DictCursor
+        )
 
-    # Query database for user
-    cursor = db.cursor()
-    query = "SELECT * FROM user WHERE username = %s AND password = %s"
-    cursor.execute(query, (username, password))
-    result = cursor.fetchone()
+        # Get username and password from form submission
+        username = request.form['username']
+        password = request.form['password']
 
-    if result:
-        # If user exists, redirect to home page
-        return redirect(url_for('/'))
+        # Query database for user
+        with conn.cursor() as cursor:
+            query = "SELECT * FROM user WHERE username = %s AND password = %s"
+            cursor.execute(query, (username, password))
+            result = cursor.fetchone()
+
+        # Close the database connection
+        conn.close()
+
+        if result:
+            # If user exists, redirect to home page
+            return redirect(url_for('index'))
+        else:
+            # If user does not exist, redirect back to login page with error message
+            return render_template('login.html', error='Invalid username or password')
     else:
-        # If user does not exist, redirect back to login page with error message
-        return redirect(url_for('index', error='Invalid username or password'))
+        # render login page
+        return render_template('login.html')
